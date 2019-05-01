bf8b70bd59b4ad4f03a1791872dad40bf225f4f8
Piotr Furmanski <>
2019-05-01 18:44:35 +0200

    another test

--- MusicBrainzTests/MapPresenterTests.swift
+++ MusicBrainzTests/MapPresenterTests.swift
@@ -32,4 +32,28 @@ class MapPresenterTests: XCTestCase {
         XCTAssertEqual(mapView.centerNumber, 1)
     }
     
+    func testShouldProperlyRemoveMark() {
+        
+        let expect = expectation(description: "Waiting response")
+        let mapView = MapViewMock()
+        let mapPresenterUnderTest = MapPresenter(service: MusicServiceMock())
+        mapPresenterUnderTest.attach(view: mapView)
+        
+        mapPresenterUnderTest.loadData(for: "warszawa") {
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertEqual(mapPresenterUnderTest.placeMarks.count, 2)
+        
+        let place = Place(type: "stadium", name: "Warszawa 1", address: "adres1",
+                              coordinates: Coordinates(latitude: "10", longitude: "10"),
+                              lifeSpan: LifeSpan(begin: "1991", end: nil, ended: nil))
+        if let placeMark = PlaceMark(place: place) {
+            mapPresenterUnderTest.remove(mark: placeMark)
+        }
+        XCTAssertEqual(mapPresenterUnderTest.placeMarks.count, 1)
+    }
+    
 }
